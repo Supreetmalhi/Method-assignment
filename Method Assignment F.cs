f423769a6ee7bfa04879a92ead6b8ecd1a46b2da
supreet Kaur Malhi <malhisupreet40@gmail.com>
2021-07-10 12:20:55 -0400

    Add files via upload

---
+++ Method Assignment F.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+
+namespace ClassMethod
+
+{
+
+    class Program
+
+    {
+
+        void sum(int num1)
+
+        {
+
+            Console.WriteLine(num1 + 10);
+
+        }
+
+        void subtract(int num1)
+
+        {
+
+            Console.WriteLine(num1 - 10);
+
+        }
+
+        void divide(int num1)
+
+        {
+
+            Console.WriteLine(num1 / 10);
+
+        }
+
+        static void Main(string[] args)
+
+        {
+
+            Program ps = new Program();
+
+            int num2;
+
+            Console.WriteLine("Enter Number : ");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            ps.sum(num2);
+
+            ps.subtract(num2);
+
+            ps.divide(num2);
+
+        }
+
+    }
+
+}
